924625ab2b763219a8d890b4a921f94c0f700d33
yuma140902 <yuma140902@gmail.com>
2021-04-04 15:32:42 +0900

    auto commit

--- s4/auto_commit.ps1
+++ s4/auto_commit.ps1
@@ -3,5 +3,6 @@ git add -N .
 git add --all
 $commit_msg = (git diff --cached --name-only)
 $commit_detail = (git diff --cached --stat)
-git commit -a -m "auto commit: ${commit_msg}" -m "${commit_detail} "
+#git commit -a -m "auto commit: ${commit_msg}" -m "${commit_detail} "
+git commit -a -m "auto commit"
 git push
