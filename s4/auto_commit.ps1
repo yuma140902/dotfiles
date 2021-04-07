576db68b953feb9515e131e6186d72b2abd3af7f
yuma140902 <yuma140902@gmail.com>
2021-04-07 17:16:34 +0900

    文字化けを抑制

--- s4/auto_commit.ps1
+++ s4/auto_commit.ps1
@@ -1,8 +1,13 @@
 git pull
 git add -N .
 git add --all
+$tmp_file = New-TemporaryFile
 $commit_msg = (git diff --cached --name-only)
 $commit_detail = (git diff --cached --stat)
+Set-Content -Encoding sjis -Path $tmp_file -Value $commit_msg
+$commit_msg = Get-Content $tmp_file
+Set-Content -Encoding sjis -Path $tmp_file -Value $commit_detail
+$commit_detail = Get-Content $tmp_file
 git commit -a -m "auto commit: ${commit_msg}" -m "${commit_detail} "
-#git commit -a -m "auto commit"
+Remove-Item -Path $tmp_file
 git push
