804fe29543b3daab366a46f0b46909eee533f2dd
yuma140902 <yuma140902@gmail.com>
2021-04-07 18:06:55 +0900

    GHaaCSを追加

--- s4/GHaaCS/ghs.ps1
+++ s4/GHaaCS/ghs.ps1
@@ -3,10 +3,27 @@ Param(
   [String]$target_repositry = ""
 )
 
-
 $repos_dir = Get-Item "~\repos"
 $auto_commit_repositries = @("local-settings", "school")
 $repositries = Get-ChildItem -Directory $repos_dir
+
+function auto_commit {
+  git pull
+  git add -N .
+  git add --all
+  $tmp_file = New-TemporaryFile
+  $commit_msg = (git diff --cached --name-only)
+  $commit_detail = (git diff --cached --stat)
+  Set-Content -Encoding sjis -Path $tmp_file -Value $commit_msg
+  $commit_msg = Get-Content $tmp_file
+  Set-Content -Encoding sjis -Path $tmp_file -Value $commit_detail
+  $commit_detail = Get-Content $tmp_file
+  git commit -a -m "auto commit: ${commit_msg}" -m "${commit_detail} "
+  Remove-Item -Path $tmp_file
+  git push
+}
+
+
 if(-Not([string]::IsNullorEmpty($target_repositry))) {
   $auto_commit_repositries = @(Join-Path $repos_dir $target_repositry)
   $repositries = @(Join-Path $repos_dir $target_repositry)
@@ -25,8 +42,12 @@ if($operation -eq "status"){
 }
 elseif($operation -eq "commit"){
   foreach($repo in $auto_commit_repositries) {
+    $name = $repo
+    $repo = Join-Path $repos_dir $name
+    Write-Host "■ ${name} ----------------------"
     Push-Location $repo
-    &..\auto_commit.ps1
+    auto_commit
     Pop-Location
+    Write-Host "---------------------------------------"
   }
 }
--- s4/hidden_task_runner.js
+++ s4/hidden_task_runner.js
@@ -1,3 +1,3 @@
 wsShell = WScript.createObject("WScript.Shell");
-retCode = wsShell.Run("pwsh -File \"" + WScript.Arguments.Item(0)+"\"",0,true);
+retCode = wsShell.Run("pwsh -File \"" + WScript.Arguments.Item(0)+"\" "+WScript.Arguments.Item(1),0,true);
 WScript.Quit(retCode);
